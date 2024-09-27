ec99eea8e40f9411f7ac22976548a261d1b059ad
Sarah Nystrom <snystrom@cityoftacoma.org>
2024-09-27 11:20:56 -0700

    Start of code

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import os
+import csv
 
+#Set variables to store data
+totalvotes = 0
+maxvotes = 0
+candidates = {}
+winner = ("")
+
+#Set path to csv file
+csvfile = "election_data.csv"
+with open(csvfile, "r") as file:
+  
